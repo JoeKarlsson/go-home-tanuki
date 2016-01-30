e99c815b461cdd73be89c0285a2da4e54a219871
Corina <corina.aj@gmail.com>
2016-01-30 13:04:46 -1000

    fix braces error

--- src/js/game/states/splash.js
+++ src/js/game/states/splash.js
@@ -11,14 +11,15 @@ splash.create = function() {
   var style = {font: 'bold 32px monospace', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle'};
   var text = splash.add.text(350,650, 'Click to start', style);
    splash.time.events.add(Phaser.Timer.SECOND * 3, addPhone, this);
-  };
-}
+
+};
 
 splash.update = function() {
   // go to game
   if ( fireButton.isDown || splash.input.activePointer.isDown) {
     this.game.state.start('game');
   }
+};
 
 module.exports = splash; // export to app.js
 function addPhone () {
