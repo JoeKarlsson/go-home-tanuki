8cc07e71cce3205d1a76c5803b6606bd729c9763
Corina <corina.aj@gmail.com>
2016-01-30 13:03:02 -1000

    add click to start message and tanuki phone appearance

--- src/js/game/states/splash.js
+++ src/js/game/states/splash.js
@@ -1,26 +1,28 @@
-var splash = {}; // corina is not entirely sure this is right
-// var cursors;
+var splash = {};
 var fireButton;
-// var leftClick;
 
 splash.preload = function() {
 
 };
 
 splash.create = function() {
-  this.add.image(0,0,'splash-1');
+  this.add.image(0,0,'splash-2');
   fireButton = splash.input.keyboard.addKey(Phaser.Keyboard.UP);
-};
+  var style = {font: 'bold 32px monospace', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle'};
+  var text = splash.add.text(350,650, 'Click to start', style);
+   splash.time.events.add(Phaser.Timer.SECOND * 3, addPhone, this);
+  };
+}
 
 splash.update = function() {
+  // go to game
   if ( fireButton.isDown || splash.input.activePointer.isDown) {
     this.game.state.start('game');
-    }
-};
-
-// go to game
+  }
 
 module.exports = splash; // export to app.js
-
+function addPhone () {
+  this.add.image(325,400, 'splash-phone');
+}
 
 // to do: add script to index page?
