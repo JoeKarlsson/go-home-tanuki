7f186d7e782261fa9825c8479007471ac82cd6aa
Kent Salcedo <kentsalcedo@gmail.com>
2016-01-30 09:37:46 -1000

    Adds all current image assets

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -2,20 +2,27 @@ var preloader = {};
 
 preloader.preload = function () {
 
+  this.load.image('ground', 'images/ground.png');
   this.game.load.image('starfield', 'images/starfield.png');
-  this.game.load.image('bullet', 'images/bullet.png');
-  this.game.load.image('enemy-green', 'images/enemy-green.png');
+
+  this.game.load.image('seat', 'images/Seat_Large_1.png');
+  this.game.load.image('shop1', 'images/Shop_Large_1.png');
+  this.game.load.image('shop2', 'images/Shop_Large_2.png');
+
   this.game.load.image('box1', 'images/GUI_Box_1.png');
+  this.game.load.image('chatBox', 'images/Chat_phone_1.png');
+
+  this.game.load.image('rock1', 'images/Rock_Large_1.png');
+  this.game.load.image('rock2', 'images/Rock_Large_2.png');
+
+  this.game.load.image('drinkSmall', 'images/Drink_Small_1.png');
   this.game.load.image('sakeSmall', 'images/Sake_Small_1.png');
   this.game.load.image('sakeLarge', 'images/Sake_Large_1.png');
   this.game.load.image('smallLeaf', 'images/Leaf_Small_1.png');
-  this.game.load.image('drinkSmall', 'images/Drink_Small_1.png');
+
   this.game.load.image('ship', 'images/tanukifly1.png');
   this.game.load.image('tanuki2', 'images/tanukifly2.png');
-  this.load.image('ground', 'images/ground.png');
-
   this.load.spritesheet('tanuki', 'images/tanukisheet.png', 115, 100, 2);
-
 
 };
 
