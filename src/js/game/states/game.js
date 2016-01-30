4beaa5889e092e8bdc5ca2adf74f9132b6ff2440
Kent Salcedo <kentsalcedo@gmail.com>
2016-01-30 12:50:06 -1000

    Adds latern obstacles, death on collide, adds sake at random heights, no event for sake collision yet

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -5,6 +5,9 @@ var cursors;
 var fireButton;
 var ground;
 var leftClick;
+var lamp;
+var smallSake;
+var drunkMeter = 10;
 
 var groundSpeed = 2; // speed of ground movement
 var skySpeed = 2; // speed of sky movement
@@ -16,15 +19,13 @@ var anchorA = 0.5; // rotational point for player
 var anchorB = 0.5; // rotational point for player
 
 var upAngle = -40; // how many degrees the player will rotate on click
-var upAngleTime = 1000; // how many seconds player will stay rotated in ms
+var upAngleTime = 100; // how many seconds player will stay rotated in ms
 
 var ACCELERATION = 600;
 var DRAG = 400;
 var MAXSPEED = 400;
 
 game.create = function () {
-  // environment gravity
-  game.physics.arcade.gravity.y = gravityForce;
 
   // The scrolling starfield
   starfield = this.starfield = game.add.tileSprite( 0, 0, 1024, 768, 'starfield' );
@@ -33,10 +34,9 @@ game.create = function () {
   player = this.player = this.game.add.sprite( 200, 300, 'ship' );
   game.physics.enable( player, Phaser.Physics.ARCADE );
   player.anchor.setTo( anchorA, anchorB );
+  player.body.gravity.y = gravityForce;
 
   // the scrolling ground
-  // note on collission not being detected on phaser 2.3 for sprites
-  // http://www.html5gamedevs.com/topic/13856-problem-with-collide-method-from-physics-arcade-in-different-versions-of-phaser/
   ground = this.ground = game.add.tileSprite(0, 656, 1024, 112, 'ground');
   ground.physicsType = Phaser.SPRITE;
   game.physics.enable( ground, Phaser.Physics.ARCADE );
@@ -46,9 +46,34 @@ game.create = function () {
   fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
   leftClick = game.input.onDown.add(flap, player);
 
+  // init lamp obstacle
+  lamp = game.add.group();
+  lamp.enableBody = true;
+  lamp.createMultiple(3,'lamp');
+  lamp.setAll('outOfBoundsKill', true);
+  lamp.setAll('checkWorldBounds', true);
+
+  this.timer = game.time.events.loop(2000, addSmallSake, this);
+
+  // init small sake
+  smallSake = game.add.group();
+  smallSake.enableBody = true;
+  smallSake.physicsType = Phaser.Physics.ARCADE;
+  smallSake.createMultiple(5,'smallSake');
+  smallSake.setAll('anchor.x', 0.5);
+  smallSake.setAll('anchor.y', 0.5);
+  smallSake.setAll('outBoundsKill', true);
+  smallSake.setAll('checkWorldBounds', true);
+
+  this.timer = game.time.events.loop(2000, addLamp, this);
+
 }; // ******** end of game create **********
 
 game.update = function () {
+  if (player.alive === false){
+    restartGame();
+  }
+
   game.physics.arcade.collide(player,ground);
 
   // scroll the ground
@@ -56,18 +81,15 @@ game.update = function () {
   game.physics.arcade.enableBody(ground);
   ground.body.allowGravity = false;
   ground.body.checkCollision = true;
-  // ground.body.collideWorldBounds = true;
   ground.body.allowGravity = false;
   ground.body.immovable = true;
   ground.body.moves = false;
-  // ground.body.enable = false;
 
   // Scroll the background
   starfield.tilePosition.x -= skySpeed;
 
   // player can collide with upper and lower bounds
   player.body.collideWorldBounds = true;
-  // game.physics.arcade.collide(player, ground, deathHandler(),game);
 
     // if player is turned up, player will correctly orient down with gravity
     if(player.angle < 90) {
@@ -79,6 +101,7 @@ game.update = function () {
       flap();
     }
 
+  game.physics.arcade.overlap(player,lamp,death,null,this);
 
 }; // ******** end of game create **********
 
@@ -88,26 +111,40 @@ function flap() {
   game.add.tween(player).to({angle: upAngle}, upAngleTime).start();
 }
 
-function deathHandler() {
-  console.log('game over');
-  // game.state.start('gameover');
+function death() {
+  if (player.alive === false){
+    return;
+  }
+  player.alive = false;
+  game.time.events.remove(this.timer);
+  lamp.forEachAlive(function(lamp){
+    lamp.body.velocity.x = 0;
+  },this);
+  starfield.tilePosition.x = 0;
+  ground.tilePosition.x = 0;
 }
 
-// function launchGreenEnemy() {
-//   var MIN_ENEMY_SPACING = 300;
-//   var MAX_ENEMY_SPACING = 3000;
-//   var ENEMY_SPEED = 300;
+function addLamp() {
+  var item = lamp.getFirstExists(false);
+  game.physics.arcade.collide(item,ground);
+  item.reset(1023, 367);
+  item.body.velocity.x = -200;
+  item.body.immovable = true;
+}
 
-//   var enemy = greenEnemies.getFirstExists(false);
-//   if (enemy) {
-//     enemy.reset(game.rnd.integerInRange(0, game.width), -20);
-//     enemy.body.velocity.x = game.rnd.integerInRange(-300, 300);
-//     enemy.body.velocity.y = ENEMY_SPEED;
-//     enemy.body.drag.x = 100;
-//   }
+function addSmallSake() {
+  var item = smallSake.getFirstExists(false);
+  item.reset(1023, game.rnd.integerInRange(10,500));
+  item.body.velocity.x = -200;
+  item.body.immovable = true;
+}
 
-//   //  Send another enemy soon
-//   game.time.events.add(game.rnd.integerInRange(MIN_ENEMY_SPACING, MAX_ENEMY_SPACING), launchGreenEnemy);
-// }
+function collectSake() {
+  console.log('collected');
+}
+
+function restartGame(){
+  game.state.start('game');
+}
 
 module.exports = game;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -14,15 +14,16 @@ preloader.preload = function () {
 
   this.game.load.image('rock1', 'images/Rock_Large_1.png');
   this.game.load.image('rock2', 'images/Rock_Large_2.png');
+  this.game.load.image('lamp', 'images/Lamp_Large_1.png');
 
   this.game.load.image('drinkSmall', 'images/Drink_Small_1.png');
-  this.game.load.image('sakeSmall', 'images/Sake_Small_1.png');
-  this.game.load.image('sakeLarge', 'images/Sake_Large_1.png');
+  this.game.load.image('smallSake', 'images/Sake_Small_1.png');
+  this.game.load.image('largeSake', 'images/Sake_Large_1.png');
   this.game.load.image('smallLeaf', 'images/Leaf_Small_1.png');
 
   this.game.load.image('ship', 'images/tanukifly1.png');
   this.game.load.image('tanuki2', 'images/tanukifly2.png');
-  this.load.spritesheet('tanuki', 'images/tanukisheet.png', 115, 100, 2);
+  // this.load.spritesheet('tanuki', 'images/tanukisheet.png', 115, 100, 2);
 
 };
 
