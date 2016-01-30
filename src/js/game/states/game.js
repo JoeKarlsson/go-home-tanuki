456f43d87ac430eca06ce636205a4ef31be05716
Joe Karlsson <joe.carlson@callmejoe.net>
2016-01-30 09:42:37 -1000

    overrriding local changes with Kents updates

--- src/js/game/states/game.js
+++ src/js/game/states/game.js
@@ -1,17 +1,8 @@
 var game = {};
 var starfield;
 var player;
-var greenEnemies;
-var cursors;
-var bank;
-var shipTrail;
-var bullets;
-var fireButton;
-var bulletTimer = 0;
-
-var ACCELERATION = 600;
-var DRAG = 400;
-var MAXSPEED = 400;
+var tanuki;
+var pipes;
 
 game.create = function () {
 
@@ -19,142 +10,74 @@ game.create = function () {
 
   starfield = this.starfield = game.add.tileSprite( 0, 0, 800, 600, 'starfield' );
 
-  //the bullet group
-  bullets = game.add.group();
-  bullets.enableBody = true;
-  bullets.physicsBodyType = Phaser.Physics.ARCADE;
-  bullets.createMultiple(30, 'bullet');
-  bullets.setAll('anchor.x', 0.5);
-  bullets.setAll('anchor.y', 1);
-  bullets.setAll('outOfBoundsKill', true);
-  bullets.setAll('checkWorldBounds', true);
+  // Set the physics system
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-  // The player
-  player = this.player = this.game.add.sprite( 400, 500, 'ship' );
-  player.anchor.setTo( 0.5, 0.5 );
-  game.physics.enable( player, Phaser.Physics.ARCADE );
-  player.body.maxVelocity.setTo( MAXSPEED, MAXSPEED );
-  player.body.drag.setTo( DRAG, DRAG );
+  // Display the tanuki on the screen
+  tanuki = this.tanuki = this.game.add.sprite(100, 245, 'tanuki');
 
-  //  The baddies!
-  greenEnemies = game.add.group();
-  greenEnemies.enableBody = true;
-  greenEnemies.physicsBodyType = Phaser.Physics.ARCADE;
-  greenEnemies.createMultiple(5, 'enemy-green');
-  greenEnemies.setAll('anchor.x', 0.5);
-  greenEnemies.setAll('anchor.y', 0.5);
-  greenEnemies.setAll('scale.x', 0.5);
-  greenEnemies.setAll('scale.y', 0.5);
-  greenEnemies.setAll('angle', 180);
-  greenEnemies.setAll('outOfBoundsKill', true);
-  greenEnemies.setAll('checkWorldBounds', true);
 
-  launchGreenEnemy();
+  //Add anchor to tanuki to animate correctly
+  tanuki.anchor.setTo(-0.2, 0.5);
 
-  //controls to play the game with
-  cursors = game.input.keyboard.createCursorKeys();
-  fireButton = game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  // Add gravity to the tanuki to make it fall
+  game.physics.arcade.enable(tanuki);
+  tanuki.body.gravity.y = 1000;
 
-  // add an emitter for the ship's trail
-  shipTrail = game.add.emitter(player.x, player.y + 10, 400 );
-  shipTrail.width = 10;
-  shipTrail.makeParticles('bullet');
-  shipTrail.setXSpeed( 30, -30 );
-  shipTrail.setYSpeed( 200, 180 );
-  shipTrail.setRotation( 50, -50 );
-  shipTrail.setAlpha( 1, 0.01, 800 );
-  shipTrail.setScale( 0.05, 0.4, 0.05, 0.4, 2000, Phaser.Easing.Quintic.Out);
-  shipTrail.start( false, 5000, 10 );
+  // Call the 'jump' function when the spacekey is hit
+  var spaceKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
+  // spaceKey.onDown.add(this.jump, this);
+  // pipes = this.pipes = game.add.group(); // Create a group
+
+  // //Create the pipes
+  // pipes.enableBody = true;  // Add physics to the group
+  // pipes.createMultiple(20, 'pipe'); // Create 20 pipes
+  // this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+
+  // // Create the score label
+  // this.score = 0;
+  // this.labelScore = game.add.text(20, 20, "0", { font: "30px Arial", fill: "#ffffff" });
+
 };
 
 game.update = function () {
   //  Scroll the background
-  starfield.tilePosition.y += 2;
+  starfield.tilePosition.x -= 2;
 
-  //  Reset the player, then check for movement keys
-  player.body.acceleration.x = 0;
-
-  if (cursors.left.isDown)
-  {
-    player.body.acceleration.x = -ACCELERATION;
-  }  else if (cursors.right.isDown)
-  {
-    player.body.acceleration.x = ACCELERATION;
+  //always angle the bird downwards
+  if (tanuki.angle < 20) {
+      tanuki.angle += 1;
   }
 
-  //  Stop at screen edges
-  if (player.x > game.width - 50) {
-    player.x = game.width - 50;
-    player.body.acceleration.x = 0;
-  }
-  if (player.x < 50) {
-    player.x = 50;
-    player.body.acceleration.x = 0;
-  }
-
-  // Fire bullet
-  if (fireButton.isDown || game.input.activePointer.isDown ) {
-    fireBullet();
-  }
-
-  //  Move ship towards mouse pointer
-  if (game.input.x < game.width - 20 &&
-      game.input.x > 20 &&
-      game.input.y > 20 &&
-      game.input.y < game.height - 20) {
-    var minDist = 200;
-    var dist = game.input.x - player.x;
-    player.body.velocity.x = MAXSPEED * game.math.clamp(dist / minDist, -1, 1);
-  }
-
-  //  Squish and rotate ship for illusion of "banking"
-  bank = player.body.velocity.x / MAXSPEED;
-  player.scale.x = 1 - Math.abs(bank) / 2;
-  player.angle = bank * 30;
-
-  //  Keep the shipTrail lined up with the ship
-  shipTrail.x = player.x;
+  // If the bird is out of the world (too high or too low), call the 'restartGame' function
+  // if (this.bird.inWorld == false)
+      // this.restartGame();
 };
 
-function fireBullet() {
-
-  // To avoid bullets from being allowed to fire too fast, set a time limit
-  if (game.time.now > bulletTimer) {
-    var BULLET_SPEED = 400;
-    var BULLET_SPACING = 250;
-
-    // Grab the first available bullet from the pool
-    var bullet = bullets.getFirstExists(false);
-
-    if (bullet) {
-      // and fire it
-      // Make the bullet come out of the tip of the ship with the correct angle
-      var bulletOffset = 20 * Math.sin(game.math.degToRad(player.angle));
-      bullet.reset( player.x + bulletOffset, player.y);
-      bullet.angle = player.angle;
-      game.physics.arcade.velocityFromAngle(bullet.angle - 90, BULLET_SPEED, bullet.body.velocity);
-      bullet.body.velocity.x += player.body.velocity.x;
-
-      bulletTimer = game.time.now + BULLET_SPACING;
-    }
-  }
+// Restart the game
+function restartGame() {
+// Start the 'main' state, which restarts the game
+  game.state.start('main');
 }
 
-function launchGreenEnemy() {
-  var MIN_ENEMY_SPACING = 300;
-  var MAX_ENEMY_SPACING = 3000;
-  var ENEMY_SPEED = 300;
+// Make the tanuki jump
+function jump() {
 
-  var enemy = greenEnemies.getFirstExists(false);
-  if (enemy) {
-    enemy.reset(game.rnd.integerInRange(0, game.width), -20);
-    enemy.body.velocity.x = game.rnd.integerInRange(-300, 300);
-    enemy.body.velocity.y = ENEMY_SPEED;
-    enemy.body.drag.x = 100;
-  }
+    //make the bird jump when it's dead.
+    if (this.bird.alive == false)
+        return;
 
-  //  Send another enemy soon
-  game.time.events.add(game.rnd.integerInRange(MIN_ENEMY_SPACING, MAX_ENEMY_SPACING), launchGreenEnemy);
+    // Add a vertical velocity to the bird
+    this.bird.body.velocity.y = -350;
+
+    // Create an animation on the bird
+    var animation = game.add.tween(this.bird);
+
+    // Set the animation to change the angle of the sprite to -20° in 100 milliseconds
+    animation.to({angle: -20}, 100);
+
+    // And start the animation
+    animation.start();
 }
 
 module.exports = game;
--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -2,9 +2,10 @@ var preloader = {};
 
 preloader.preload = function () {
   this.game.load.image('starfield', 'images/starfield.png');
-  this.game.load.image('ship', 'images/player.png');
+  this.game.load.image('tanuki', 'images/player.png');
   this.game.load.image('bullet', 'images/bullet.png');
-  this.game.load.image('enemy-green', 'images/enemy-green.png')
+  this.game.load.image('enemy-green', 'images/enemy-green.png');
+  this.game.load.image('pipe', 'images/pipe.png');
 
 };
 
