cd0d0099a1e5dc7cd12943f63927b2637c8c7fec
Kent Salcedo <kentsalcedo@gmail.com>
2016-01-30 08:33:47 -1000

    Adds all image assets. Working moving ground and sky. Able to input through click and spacebar. Adds player flap motion. Player contrained to visable area

--- src/js/game/states/preloader.js
+++ src/js/game/states/preloader.js
@@ -1,10 +1,21 @@
 var preloader = {};
 
 preloader.preload = function () {
+
   this.game.load.image('starfield', 'images/starfield.png');
-  this.game.load.image('ship', 'images/player.png');
   this.game.load.image('bullet', 'images/bullet.png');
-  this.game.load.image('enemy-green', 'images/enemy-green.png')
+  this.game.load.image('enemy-green', 'images/enemy-green.png');
+  this.game.load.image('box1', 'images/GUI_Box_1.png');
+  this.game.load.image('sakeSmall', 'images/Sake_Small_1.png');
+  this.game.load.image('sakeLarge', 'images/Sake_Large_1.png');
+  this.game.load.image('smallLeaf', 'images/Leaf_Small_1.png');
+  this.game.load.image('drinkSmall', 'images/Drink_Small_1.png');
+  this.game.load.image('ship', 'images/tanukifly1.png');
+  this.game.load.image('tanuki2', 'images/tanukifly2.png');
+  this.load.image('ground', 'images/ground.png');
+
+  this.load.spritesheet('tanuki', 'images/tanukisheet.png', 115, 100, 2);
+
 
 };
 
