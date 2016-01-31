3dce58eb09103bdeebf7c801ff152c9d28fa45e5
Sarah Kurisu <sarah.kurisu@gmail.com>
2016-01-30 18:40:38 -1000

    added animation for title, text and tanuki

--- src/js/game/states/splash.js
+++ src/js/game/states/splash.js
@@ -1,49 +1,75 @@
-var splash = {};
+var splash = function  () {
+  
+};
+
 var fireButton;
 var shops;
 var text;
+var phone;
+var message;
+var title;
+var tanuki;
 var tween;
 var transitionSpeed = 2;
 var style = {font: 'bold 32px monospace', fill: '#fff', boundsAlignH: 'center', boundsAlignV: 'middle'};
-splash.preload = function() {
+splash.prototype.preload = function() {
 
 };
 
-splash.create = function() {
-  this.starfield = splash.add.tileSprite( 0, 0, 1024, 768, 'starfield' );
-  shops = splash.add.sprite( -100, 0, 'shop' );
-
-  tween = splash.add.tween(shops);
-  tween.to({x:-1240}, 2000, 'Linear', true);
+splash.prototype.create = function() {
+  this.starfield = this.add.tileSprite( 0, 0, 1024, 768, 'starfield' );
+  shops = this.add.sprite( -100, 0, 'shop' );
 
 
-  this.add.image(0,0,'splash-1');
-  fireButton = splash.input.keyboard.addKey( Phaser.Keyboard.UP );
-  text = splash.add.text( 350, 650, 'Click to start', style );
-   splash.time.events.add( Phaser.Timer.SECOND * 3, addPhone, this );
-   splash.time.events.add( Phaser.Timer.SECOND * 6, addMessage, this );
-   splash.time.events.add( Phaser.Timer.SECOND * 6.5, addTitle, this );
+  tanuki = this.add.image(0,0,'splash-1');
+  fireButton = this.input.keyboard.addKey( Phaser.Keyboard.UP );
+  text = this.add.text( 350, 650, 'Click to start', style );
+   this.time.events.add( Phaser.Timer.SECOND * 3, this.addPhone, this );
+   this.time.events.add( Phaser.Timer.SECOND * 6, this.addMessage, this );
+   this.time.events.add( Phaser.Timer.SECOND * 6.5, this.addTitle, this );
 };
 
-splash.update = function() {
-  if ( fireButton.isDown || splash.input.activePointer.isDown ) {
+splash.prototype.update = function() {
+  var self = this;
+  if ( fireButton.isDown || this.input.activePointer.isDown ) {
+    tween = this.add.tween(shops);
+    var tweenText = this.add.tween(text);
+    var tweenPhone = this.add.tween(self.addPhone);
+    var tweenMessage = this.add.tween('splash-message-box');
+    var tweenTitle = this.add.tween('splash-text');
+    var tweenSplash = this.add.tween(tanuki)
+
+    tweenText.to({x:-300}, 1000, 'Linear', true);
+    tweenPhone.to({x:-300}, 1000, 'Linear', true);
+    tweenMessage.to({x:-300}, 1000, 'Linear', true);
+    tweenTitle.to({x:-300}, 1000, 'Linear', true);
+    tweenSplash.to({x:-1400}, 1000, 'Linear', true);
+    tween.to({x:-1440}, 1000, 'Linear', true).onComplete.add(function () {
+      console.log('game');
+      self.game.state.start('game');
+    });
     // go to game
-    this.game.state.start( 'game' );
+    // this.game.state.start( 'game' );
   }
 };
 
 module.exports = splash; // export to app.js
 
 // helper functions
-function addPhone () {
+// 
+// function gameStart () {
+//   this.game.state.start('game');
+// }
+
+splash.prototype.addPhone = function () {
   this.add.image( 325, 400, 'splash-phone');
 }
 
-function addMessage () {
+splash.prototype.addMessage = function () {
     this.add.image( 25, 260, 'splash-message-box' );
 }
 
-function addTitle () {
+splash.prototype.addTitle = function () {
     this.add.image( 175,50, 'splash-text' );
 }
 
